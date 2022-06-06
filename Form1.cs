8540466889875d47c93fd939b52744e1223e9fe6
rmmartinez24 <104732814+rmmartinez24@users.noreply.github.com>
2022-06-05 20:41:53 -0700

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LuckyNumber
+{
+    public partial class Form1 : Form
+    {
+        //veriables
+        int year;
+        int month;
+        int day;
+        int luckyNumber;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //year
+            int birthYear = 1930;
+            for (int i = 1930; i <= 2021; i++)
+            {
+                comboBox1.Items.Add(birthYear);
+                birthYear += 1;
+            }
+            
+            //month
+            int birthMonth = 1;
+            for (int i = 1; i < 13; i++)
+            {
+                comboBox2.Items.Add(birthMonth);
+                birthMonth++;
+            }
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            comboBox3.Items.Clear();
+
+            int[] monthDays = new int[] { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+            int daysInMonth = monthDays[comboBox2.SelectedIndex];
+            for (int i = 1; i < daysInMonth + 1; i++)
+            {
+                comboBox3.Items.Add(i);
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedIndex < 0 || comboBox2.SelectedIndex < 0 || comboBox3.SelectedIndex < 0)
+            {
+                MessageBox.Show("You must fill out the form to get a lucky number!");
+            }
+            else
+            {
+
+                year = int.Parse(comboBox1.SelectedItem.ToString());
+                month = int.Parse(comboBox2.SelectedItem.ToString());
+                day = int.Parse(comboBox3.SelectedItem.ToString());
+
+                luckyNumber = (year / day) * ((month / year) ^ 2);
+
+                //hide from1 
+                this.Hide();
+                //created a instance of results form
+                Form2 f2 = new Form2();
+                //show form2
+                f2.luckyNumberLabel1.Text = luckyNumber.ToString();
+                f2.ShowDialog();
+                f2 = null;
+
+                //show form1 after closing from2
+                this.Show();
+
+
+            }
+        }
+    }
+}
---
+++ Form2.Designer.cs
@@ -0,0 +1,80 @@
+﻿namespace LuckyNumber
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.luckyNumberLabel1 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // luckyNumberLabel1
+            // 
+            this.luckyNumberLabel1.AutoSize = true;
+            this.luckyNumberLabel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 40F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.luckyNumberLabel1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.luckyNumberLabel1.Location = new System.Drawing.Point(84, 107);
+            this.luckyNumberLabel1.Name = "luckyNumberLabel1";
+            this.luckyNumberLabel1.Size = new System.Drawing.Size(0, 63);
+            this.luckyNumberLabel1.TabIndex = 0;
+            this.luckyNumberLabel1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(82, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(169, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Your Lucky Number Is:";
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(343, 252);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.luckyNumberLabel1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Name = "Form2";
+            this.Text = "Lucky Number Results";
+            this.Load += new System.EventHandler(this.Form2_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        public System.Windows.Forms.Label luckyNumberLabel1;
+        private System.Windows.Forms.Label label1;
+    }
+}
